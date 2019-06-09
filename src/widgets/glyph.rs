667467d1ea13c2077c9dd50b691d6494af62add2
Colin Rofls <colin@cmyr.net>
2019-06-09 14:22:16 +0200

    Draw names under glyphs

--- src/widgets/glyph.rs
+++ src/widgets/glyph.rs
@@ -2,15 +2,17 @@
 
 use kurbo::{Affine, BezPath, Rect, Shape, Vec2};
 use norad::glyph::{Contour, ContourPoint, Glyph, PointType};
-use piet::{FillRule, RenderContext};
+use piet::{FillRule, FontBuilder, RenderContext, Text, TextLayout, TextLayoutBuilder};
+use piet_common::Piet;
+
 use druid::{
     BoxConstraints, Geometry, HandlerCtx, Id, LayoutResult, LayoutCtx, MouseEvent, PaintCtx, Ui, Widget,
 };
 
-//const CELL_BG_COLOR: u32 =  0x_24_24_24_ff;
+const TEXT_BG_COLOR: u32 =  0x27_28_22_DD;
 const GLYPH_COLOR: u32 =  0x6a_6a_6a_ff;
 const HIGHLIGHT_COLOR: u32 =  0xfa_fa_fa_ff;
-const ON_CLICK_COLOR: u32 =  0x_24_24_84_ff;
+const ON_CLICK_COLOR: u32 =  0x_F4_24_84_ff;
 
 #[allow(dead_code)]
 pub struct GlyphWidget {
@@ -46,6 +48,10 @@ impl Widget for GlyphWidget {
         if is_active {
             let brush = ctx.render_ctx.solid_brush(ON_CLICK_COLOR).unwrap();
             ctx.render_ctx.fill(rect, &brush, FillRule::NonZero);
+        } else {
+            //NOTE: uncomment to always draw background
+            //let bg_color = ctx.render_ctx.solid_brush(0x_FF_22_44_4F).unwrap();
+            //ctx.render_ctx.fill(rect, &bg_color, FillRule::NonZero);
         }
 
         let bb = self.path.bounding_box();
@@ -62,15 +68,35 @@ impl Widget for GlyphWidget {
                                  x as f64 + l_pad,
                                  (height +  y) as f64 - baseline,
         ]);
-        let fill_color = if is_active { HIGHLIGHT_COLOR } else { GLYPH_COLOR };
-        let fill = ctx.render_ctx.solid_brush(fill_color).unwrap();
+
+
+        let glyph_body_color = if is_active { HIGHLIGHT_COLOR } else { GLYPH_COLOR };
+        let fill = ctx.render_ctx.solid_brush(glyph_body_color).unwrap();
         ctx.render_ctx.fill(affine * &self.path, &fill, FillRule::NonZero);
+
         if is_hot {
             let outline_color = ctx.render_ctx.solid_brush(HIGHLIGHT_COLOR).unwrap();
             //eprintln!("{} {:?} x{} {:?}", self.glyph.name, bb, scale, rect);
-            ctx.render_ctx.stroke(affine * &self.path, &outline_color, 0.5, None);
-            ctx.render_ctx.stroke(rect, &outline_color, 0.5, None);
+            ctx.render_ctx.stroke(affine * &self.path, &outline_color, 1.0, None);
+            ctx.render_ctx.stroke(rect, &outline_color, 1.0, None);
         }
+
+        // draw the glyph name:
+        let font_size = 12.0;
+        let name_color = if is_hot { HIGHLIGHT_COLOR } else { GLYPH_COLOR };
+        let text = get_text_layout(&mut ctx.render_ctx, &self.glyph.name, font_size);
+        let xpos = geom.pos.0 + (geom.size.0 - text.width() as f32) * 0.5;
+        let ypos = geom.pos.1 + geom.size.1 - font_size * 0.25;
+        let pos = (xpos, ypos);
+
+        //draw a semi-translucent background
+        let text_bg_rect = Rect::from_origin_size((pos.0 as f64, (pos.1 - font_size * 0.75) as f64).into(), (text.width(), font_size as f64).into());
+        let text_bg_color = ctx.render_ctx.solid_brush(TEXT_BG_COLOR).unwrap();
+        ctx.render_ctx.fill(text_bg_rect, &text_bg_color, FillRule::NonZero);
+
+        // draw the text
+        let brush = ctx.render_ctx.solid_brush(name_color).unwrap();
+        ctx.render_ctx.draw_text(&text, pos, &brush);
     }
 
     fn layout(
@@ -99,6 +125,20 @@ impl Widget for GlyphWidget {
         ctx.invalidate();
         true
     }
+}
+
+fn get_text_layout<'a, 'b>(piet: &'a mut Piet, text: &'b str, font_size: f32) -> <Piet<'a> as RenderContext>::TextLayout {
+    let font = piet
+        .text()
+        .new_font_by_name("Avenir Next Condensed", font_size)
+        .unwrap()
+        .build()
+        .unwrap();
+    piet.text()
+        .new_text_layout(&font, text)
+        .unwrap()
+        .build()
+        .unwrap()
 }
 
 pub fn path_for_glyph(glyph: &Glyph) -> BezPath {
