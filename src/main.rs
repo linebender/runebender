6e2ea97505407b3028d97e6dd19e9d25c9c3c909
Colin Rofls <colin@cmyr.net>
2019-09-25 16:33:34 -0400

    Add basic app, setup open dialog

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 //! A font editor.
 
+mod menus;
+
+use druid::widget::{Align, Label, Padding};
+use druid::{AppLauncher, LocalizedString, Widget, WindowDesc};
+
+type AppState = u32;
+
 fn main() {
-    unimplemented!()
+    let main_window = WindowDesc::new(make_ui)
+        .title(LocalizedString::new("Runebender"))
+        .menu(menus::make_menu());
+
+    AppLauncher::with_window(main_window)
+        .use_simple_logger()
+        .launch(0)
+        .expect("launch failed");
+}
+
+fn make_ui() -> impl Widget<AppState> {
+    let text = LocalizedString::new("Fontville!");
+    let label = Label::new(text);
+    Align::centered(Padding::uniform(5.0, label))
 }
