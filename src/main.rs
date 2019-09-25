a5bfb768af85eeb7e78a0533d3f57fefc5871aac
Colin Rofls <colin@cmyr.net>
2019-09-25 14:11:19 -0400

    Fresh start

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+//! A font editor.
+
+fn main() {
+    unimplemented!()
+}
--- src/widgets/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Custom UI widgets for druid.
-
-mod glyph;
-mod grid;
-mod editor;
-
-pub use editor::GlyphEditor;
-pub use glyph::GlyphWidget;
-pub use grid::Grid;
