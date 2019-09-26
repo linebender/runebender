5811738bd1a91623db597b81aaa0ac4414b3e237
Colin Rofls <colin@cmyr.net>
2019-09-26 13:05:31 -0400

    Add root widget and dummy grid view

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ mod data;
 mod menus;
 mod widgets;
 
-use druid::widget::{Align, DynLabel, Padding};
+use druid::widget::{Align, Column, DynLabel, Padding, Scroll, SizedBox};
 use druid::{AppLauncher, LocalizedString, Widget, WindowDesc};
 
 use data::AppState;
-use widgets::Controller;
+use widgets::{Controller, GlyphGrid};
 
 fn main() {
     let main_window = WindowDesc::new(make_ui)
@@ -22,11 +22,20 @@ fn main() {
 }
 
 fn make_ui() -> impl Widget<AppState> {
-    let label = DynLabel::new(|data: &AppState, _| {
-        format!(
-            "{:?}",
-            data.file.as_ref().map(|obj| format!("{:?}", &obj.path))
-        )
+    let mut col = Column::new();
+    let label = DynLabel::new(|data: &AppState, _| match data.file.as_ref() {
+        Some(font_obj) => font_obj
+            .object
+            .font_info
+            .as_ref()
+            .and_then(|info| info.family_name.clone())
+            .unwrap_or("Unknown".to_string()),
+        None => "New Font".to_string(),
     });
-    Controller::new(Align::centered(Padding::uniform(5.0, label)))
+    col.add_child(
+        SizedBox::new(Align::centered(Padding::uniform(5.0, label))).height(80.),
+        0.,
+    );
+    col.add_child(Scroll::new(GlyphGrid::new()).vertical(), 1.);
+    Controller::new(col)
 }
