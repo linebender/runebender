2c68747d2a0c2ac4db2b36d576bb5c8b959b1681
Colin Rofls <colin@cmyr.net>
2019-09-30 16:08:01 -0400

    Cleanup initial grid impl
    - use Lens2
    - handle over & active
    - cleaner data types

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ mod widgets;
 use druid::widget::{Align, Column, DynLabel, Padding, Scroll, SizedBox};
 use druid::{AppLauncher, LocalizedString, Widget, WindowDesc};
 
-use data::AppState;
+use data::{lenses, AppState};
+use lens2::Lens2Wrap;
+
 use widgets::{Controller, GlyphGrid};
 
 fn main() {
@@ -24,19 +26,21 @@ fn main() {
 
 fn make_ui() -> impl Widget<AppState> {
     let mut col = Column::new();
-    let label = DynLabel::new(|data: &AppState, _| match data.file.as_ref() {
-        Some(font_obj) => font_obj
+    let label = DynLabel::new(|data: &AppState, _| {
+        data.file
             .object
             .font_info
             .as_ref()
             .and_then(|info| info.family_name.clone())
-            .unwrap_or("Unknown".to_string()),
-        None => "New Font".to_string(),
+            .unwrap_or_else(|| "Untitled".to_string())
     });
     col.add_child(
-        SizedBox::new(Align::centered(Padding::uniform(5.0, label))).height(60.),
+        SizedBox::new(Align::centered(Padding::uniform(5.0, label))).height(40.),
         0.,
     );
-    col.add_child(Scroll::new(GlyphGrid::new()).vertical(), 1.);
+    col.add_child(
+        Scroll::new(Lens2Wrap::new(GlyphGrid::new(), lenses::app_state::Ufo)).vertical(),
+        1.,
+    );
     Controller::new(col)
 }
