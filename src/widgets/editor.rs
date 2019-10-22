33115c32b3d70d06d937fed75a2ea9ccbbe96d33
Colin Rofls <colin@cmyr.net>
2019-10-22 12:51:56 -0400

    Cleanup

--- src/widgets/editor.rs
+++ src/widgets/editor.rs
@@ -1,10 +1,8 @@
 //! the main editor widget.
 
-use druid::kurbo::{Affine, Rect, Shape, Size};
-use druid::piet::RenderContext;
+use druid::kurbo::Size;
 use druid::{
-    theme, BaseState, BoxConstraints, Data, Env, Event, EventCtx, LayoutCtx, PaintCtx, UpdateCtx,
-    Widget,
+    BaseState, BoxConstraints, Data, Env, Event, EventCtx, LayoutCtx, PaintCtx, UpdateCtx, Widget,
 };
 
 use norad::GlyphName;
@@ -24,7 +22,7 @@ impl Editor {
 }
 
 impl Widget<EditorState> for Editor {
-    fn paint(&mut self, ctx: &mut PaintCtx, state: &BaseState, data: &EditorState, env: &Env) {
+    fn paint(&mut self, ctx: &mut PaintCtx, state: &BaseState, data: &EditorState, _env: &Env) {
         //TODO: replacement for missing glyphs
         draw::draw_session(
             ctx,
@@ -34,25 +32,6 @@ impl Widget<EditorState> for Editor {
             &data.session,
             &data.ufo,
         );
-        //let path = crate::data::get_bezier(&data.session.name, &data.ufo, None).unwrap_or_default();
-        //let bb = path.bounding_box();
-        //let geom = Rect::ZERO.with_size(state.size());
-        //let scale = geom.height() as f64 / data.metrics.units_per_em;
-        //let scale = scale * 0.85; // some margins around glyphs
-        //let scaled_width = bb.width() * scale as f64;
-        //let l_pad = ((geom.width() as f64 - scaled_width) / 2.).round();
-        //let baseline = (geom.height() * 0.16) as f64;
-        //let affine = Affine::new([
-        //scale as f64,
-        //0.0,
-        //0.0,
-        //-scale as f64,
-        //l_pad,
-        //geom.height() - baseline,
-        //]);
-
-        //ctx.render_ctx
-        //.fill(affine * &*path, &env.get(theme::FOREGROUND_DARK));
     }
 
     fn layout(
