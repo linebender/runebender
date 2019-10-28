7eea22747b2767b255144becc0fa739395f50a29
Colin Rofls <colin@cmyr.net>
2019-10-28 12:03:42 -0400

    Set initial zoom level to fit contents to window

--- src/widgets/scroll_zoom.rs
+++ src/widgets/scroll_zoom.rs
@@ -15,7 +15,6 @@ const MAX_ZOOM: f64 = 50.;
 pub struct ScrollZoom<T: Widget<EditorState>> {
     mouse: Point,
     child: Scroll<EditorState, T>,
-    zoom: f64,
     is_setup: bool,
 }
 
@@ -24,12 +23,11 @@ impl<T: Widget<EditorState>> ScrollZoom<T> {
         ScrollZoom {
             child: Scroll::new(inner),
             mouse: Point::ZERO,
-            zoom: 1.0,
             is_setup: false,
         }
     }
 
-    fn zoom(&mut self, delta: Vec2, size: Size) {
+    fn zoom(&mut self, data: &mut EditorState, delta: Vec2, size: Size) {
         let delta = most_significant_axis(delta);
         // deltas are big, so we scale them down
         let delta = delta.round() * 0.001;
@@ -37,8 +35,9 @@ impl<T: Widget<EditorState>> ScrollZoom<T> {
             return;
         }
 
-        let next_zoom = (self.zoom + delta).min(MAX_ZOOM).max(MIN_ZOOM);
-        let delta_zoom = next_zoom / self.zoom;
+        let last_zoom = data.session.viewport.zoom;
+        let next_zoom = (last_zoom + delta).min(MAX_ZOOM).max(MIN_ZOOM);
+        let delta_zoom = next_zoom / last_zoom;
 
         // prevents jitter when we're near our max or min zoom levels
         if (delta_zoom).abs() < 0.001 {
@@ -50,24 +49,37 @@ impl<T: Widget<EditorState>> ScrollZoom<T> {
         let next_off = scroll_off * delta_zoom;
         let delta_off = next_off - scroll_off;
         self.child.scroll(delta_off, size);
-        self.zoom = next_zoom;
+        data.session.viewport.zoom = next_zoom;
     }
 
     /// center the glyph on the canvas
-    fn setup(&mut self, data: &EditorState, view_size: Size) {
+    fn set_initial_scroll(&mut self, data: &EditorState, view_size: Size) {
         // set scroll offsets so that the work is centered on load
         let work_rect = glyph_rect(data);
+        let canvas_size = CANVAS_SIZE * data.session.viewport.zoom;
+        let work_size = work_rect.size() * data.session.viewport.zoom;
 
         // the top left of our glyph's layout rect
-        let x_off = CANVAS_SIZE.width / 2. - work_rect.width() / 2.;
-        let y_off = CANVAS_SIZE.height / 2. - work_rect.height() / 2.;
+        let x_off = canvas_size.width / 2. - work_size.width / 2.;
+        let y_off = canvas_size.height / 2. - work_size.height / 2.;
 
-        let bonus_x = (view_size.width - work_rect.width()) / 2.;
-        let bonus_y = (view_size.height - work_rect.height()) / 2.;
+        // the center of our glyph's metric bounds
+        let bonus_x = (view_size.width - work_size.width) / 2.;
+        let bonus_y = (view_size.height - work_size.height) / 2.;
 
         let off = Vec2::new(x_off - bonus_x, y_off - bonus_y);
+        let delta_off = off - self.child.offset();
+        self.child.scroll(delta_off, view_size);
+    }
 
-        self.child.scroll(off - self.child.offset(), view_size);
+    fn set_initial_zoom(&mut self, data: &mut EditorState, view_size: Size) {
+        let work_size = glyph_rect(data).size();
+        let fit_ratio =
+            (work_size.width / view_size.width).max(work_size.height / view_size.height);
+        if fit_ratio > 1.0 {
+            let new_zoom = 0.9 / fit_ratio;
+            data.session.viewport.zoom = new_zoom;
+        }
     }
 }
 
@@ -88,38 +100,36 @@ impl<T: Widget<EditorState>> Widget<EditorState> for ScrollZoom<T> {
     ) -> Size {
         let size = self.child.layout(ctx, bc, data, env);
         if !self.is_setup {
-            self.setup(data, size);
+            self.set_initial_scroll(data, size);
             self.is_setup = true;
         }
         size
     }
 
     fn event(&mut self, event: &Event, ctx: &mut EventCtx, data: &mut EditorState, env: &Env) {
-        if let Event::MouseMoved(mouse) = event {
-            self.mouse = mouse.pos;
-            ctx.request_focus();
-        }
-
         match event {
+            Event::Size(size) if !self.is_setup => {
+                self.set_initial_zoom(data, *size);
+                ctx.request_focus();
+            }
+            Event::MouseMoved(mouse) => {
+                self.mouse = mouse.pos;
+            }
             Event::Wheel(wheel) if wheel.mods.meta => {
-                self.zoom(wheel.delta, ctx.size());
-                data.session.viewport.zoom = self.zoom;
+                self.zoom(data, wheel.delta, ctx.size());
                 self.child.reset_scrollbar_fade(ctx);
                 ctx.set_handled();
                 ctx.invalidate();
                 return;
             }
-
             // for debugging zoom:
             Event::KeyUp(key) if key.unmod_text() == Some("j") => {
-                self.zoom(Vec2::new(50., 0.), ctx.size());
-                data.session.viewport.zoom = self.zoom;
+                self.zoom(data, Vec2::new(50., 0.), ctx.size());
                 self.child.reset_scrollbar_fade(ctx);
                 ctx.invalidate();
             }
             Event::KeyUp(key) if key.unmod_text() == Some("k") => {
-                self.zoom(Vec2::new(-50., 0.), ctx.size());
-                data.session.viewport.zoom = self.zoom;
+                self.zoom(data, Vec2::new(-50., 0.), ctx.size());
                 self.child.reset_scrollbar_fade(ctx);
                 ctx.invalidate();
             }
@@ -137,9 +147,6 @@ impl<T: Widget<EditorState>> Widget<EditorState> for ScrollZoom<T> {
         env: &Env,
     ) {
         self.child.update(ctx, old, new, env);
-        //if Some(new.session.viewport.zoom) != old.map(|o| o.session.viewport.zoom) {
-        //self.child.reset_scrollbar_fade();
-        //}
     }
 }
 
