08959b55d635b54958760fd0c6b942e006aa0a93
Eli Heuer <elih@protonmail.com>
2021-07-05 00:11:23 -0700

    Update coordinate pane layout padding and spacers (#257)

--- runebender-lib/src/widgets/coord_pane.rs
+++ runebender-lib/src/widgets/coord_pane.rs
@@ -133,13 +133,15 @@ fn build_widget() -> impl Widget<CoordinateSelection> {
                 .with_child(
                     Label::new("x")
                         .with_font(coord_label_font.clone())
-                        .with_text_color(theme::SECONDARY_TEXT_COLOR),
+                        .with_text_color(theme::SECONDARY_TEXT_COLOR)
+                        .padding((0.0, 0.0, 0.0, 8.0)),
                 )
                 .with_child(
                     EditableLabel::parse()
                         .with_font(theme::UI_DETAIL_FONT)
                         .lens(point_x_lens)
-                        .fix_width(40.0),
+                        .fix_width(40.0)
+                        .padding((0.0, 0.0, 0.0, 8.0)),
                 ),
         )
         .with_child(
@@ -167,14 +169,16 @@ fn build_widget() -> impl Widget<CoordinateSelection> {
                     .with_child(
                         Label::new("w")
                             .with_font(coord_label_font.clone())
-                            .with_text_color(theme::SECONDARY_TEXT_COLOR),
+                            .with_text_color(theme::SECONDARY_TEXT_COLOR)
+                            .padding((-0.0, 0.0, 0.0, 8.0)),
                     )
-                    .with_spacer(4.0)
+                    .with_spacer(0.0)
                     .with_child(
                         EditableLabel::parse()
                             .with_font(theme::UI_DETAIL_FONT)
                             .lens(size_width_lens)
-                            .fix_width(40.0),
+                            .fix_width(40.0)
+                            .padding((0.0, 0.0, 0.0, 8.0)),
                     ),
             )
             .with_child(
@@ -184,7 +188,7 @@ fn build_widget() -> impl Widget<CoordinateSelection> {
                             .with_font(coord_label_font)
                             .with_text_color(theme::SECONDARY_TEXT_COLOR),
                     )
-                    .with_spacer(4.0)
+                    .with_spacer(0.0)
                     .with_child(
                         EditableLabel::parse()
                             .with_font(theme::UI_DETAIL_FONT)
@@ -200,7 +204,7 @@ fn build_widget() -> impl Widget<CoordinateSelection> {
         .with_child(coord_picker)
         .with_child(coord_editor)
         .with_child(bbox_info)
-        .padding(4.0);
+        .padding(8.0);
 
     // if we have any points selected, show the numerical adjust widget, else an empty widget
     Either::new(|d, _| d.count != 0, picker_and_editor, SizedBox::empty())
